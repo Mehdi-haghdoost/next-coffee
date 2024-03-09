550cbd5fe783b0c7ff29265d447953a0a366ba04
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-03-09 17:32:48 +0330

    styling and coding slide.js component

--- components/templates/Index/Slider.js
+++ components/templates/Index/Slider.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from '@/styles/Slider.module.css'
+import { Navigation } from 'swiper/modules';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -9,10 +10,33 @@ import 'swiper/css/navigation';
 function Slider() {
   return (
     <>
-    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-      <SwiperSlide>Slide 1</SwiperSlide>
-    </Swiper>
-  </>
+      <Swiper loop={true} navigation={true} modules={[Navigation]} className={styles.swiper} >
+        <SwiperSlide
+         className={styles.swiper_slide} 
+         style={{backgroundImage : 'url("/images/carousel-1.jpg")'}}
+         >
+          <div class="carousel-item active">
+            <div className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
+              <h2 class="text-primary font-weight-medium m-0">We Have Been Serving</h2>
+              <h1 class="display-1 text-white m-0">COFFEE</h1>
+              <h2 class="text-white m-0">* SINCE 1950 *</h2>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide 
+        className={styles.swiper_slide}
+        style={{backgroundImage : 'url("/images/carousel-2.jpg")'}}
+        >
+          <div class="carousel-item ">
+            <div className={`${styles.slider_caption} d-flex flex-column align-items-center justify-content-center`}>
+              <h2 class="text-primary font-weight-medium m-0">We Have Been Serving</h2>
+              <h1 class="display-1 text-white m-0">COFFEE</h1>
+              <h2 class="text-white m-0">* SINCE 1950 *</h2>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Slider from '@/components/templates/Index/Slider'
 import React from 'react'
 
 function Home() {
   return (
     <>
+    <Slider />
     </>
   )
 }
