a9973f829ea12d415fe9b4a9d21f6cd8b4319339
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-03-09 17:10:43 +0330

    coding swiper.js component

--- components/templates/Index/Slider.js
+++ components/templates/Index/Slider.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import styles from '@/styles/Slider.module.css'
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
 function Slider() {
   return (
-    <div>Slider</div>
+    <>
+    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+      <SwiperSlide>Slide 1</SwiperSlide>
+    </Swiper>
+  </>
   )
 }
 
