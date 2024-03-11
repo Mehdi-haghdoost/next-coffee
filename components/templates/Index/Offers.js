9e96c07f0db2c0ec372f6da0d9d0a9d959e0c9b9
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-03-11 14:44:16 +0330

    coding Offers.js

--- components/templates/Index/Offers.js
+++ components/templates/Index/Offers.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Offers() {
+
+  const [email, setEmail] = useState("")
+
+  const addEmail = async (event) => {
+    event.preventDefault()
+
+    const res = await fetch('http://localhost:4000/newsLetters', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email })
+    })
+
+    if (res.status === 201) {
+      alert('you joined successfully')
+      setEmail('')
+    }
+
+  }
+
   return (
-    <div>Offers</div>
+    <div class="offer container-fluid my-5 py-5 text-center position-relative overlay-top overlay-bottom">
+      <div class="container py-5">
+        <h1 class="display-3 text-primary mt-3">50% OFF</h1>
+        <h1 class="text-white mb-3">Sunday Special Offer</h1>
+        <h4 class="text-white font-weight-normal mb-4 pb-3">Only for Sunday from 1st Jan to 30th Jan 2045</h4>
+        <form class="form-inline justify-content-center mb-4">
+          <div class="input-group">
+            <input type="text" class="form-control p-4" placeholder="Your Email" style={{ height: '60px' }}
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <div class="input-group-append">
+              <button class="btn btn-primary font-weight-bold px-4" type="submit"
+                onClick={addEmail}
+              >JOIN</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
