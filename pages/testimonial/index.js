f6ae9a1b2f466508357979a9caf96bb0018f0682
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-03-14 17:02:50 +0330

    coding testimonial

--- pages/testimonial/index.js
+++ pages/testimonial/index.js
@@ -1,9 +1,28 @@
+import PageHeader from '@/components/modules/PageHeader/PageHeader'
+import Comments from '@/components/templates/Testimonial/Comments'
 import React from 'react'
 
-function Testimonial() {
+function Testimonial({ data }) {
   return (
-    <h1>Testimonial Page</h1>
+
+    <>
+      <PageHeader />
+      <Comments data={data} />
+    </>
   )
+}
+
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:4000/comments')
+  const data = await res.json()
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 60 * 60 * 12 // second
+  }
 }
 
 export default Testimonial
