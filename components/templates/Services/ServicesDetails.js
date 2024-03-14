1b90692768f72e214145474ec09062c5e28cb6eb
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-03-14 17:06:19 +0330

    coding  ServicesDetails.js

--- components/templates/Services/ServicesDetails.js
+++ components/templates/Services/ServicesDetails.js
@@ -1,8 +1,27 @@
+import ServiceItem from '@/components/modules/ServiceItem/ServiceItem'
 import React from 'react'
 
-function ServicesDetails() {
+function ServicesDetails({data}) {
+
   return (
-    <div>ServicesDetails</div>
+    <div class="container-fluid pt-5">
+      <div class="container">
+        <div class="section-title">
+          <h4 class="text-primary text-uppercase" style={{letterSpacing: '5px'}}  >Our Services</h4>
+          <h1 class="display-4">Fresh &amp; Organic Beans</h1>
+        </div>
+        <div class="row">
+          {
+            data?.map((service) => (
+              <ServiceItem title={service.title}
+              desc={service.desc}
+              image={service.img}
+              icon={service.icon} />
+            ))
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
