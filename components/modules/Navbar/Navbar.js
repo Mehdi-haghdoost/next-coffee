fb785b024f37b0168bbd65d0796fb1e51b2f06ca
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-03-16 20:19:59 +0330

    coding search in Navbar fot searching Product

--- components/modules/Navbar/Navbar.js
+++ components/modules/Navbar/Navbar.js
@@ -1,21 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '@/styles/Navbar.module.css'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
+// Fontawesome
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+
 
 function Navbar() {
 
+  const [search, setSearch] = useState('')
+
   const route = useRouter()
+
+  useEffect(() => {
+    setSearch(route.query.q)
+    setSearch('')
+  }, [])
+
+
+  function searchHandlerWithIcon() {
+    if (search.trim()) {
+      route.push(`/search?q=${search}`)
+      setSearch('')
+    }
+  }
+
+  function searchHandlerWithEnter(event) {
+    if (event.keyCode === 13) {
+      searchHandlerWithIcon()
+    }
+  }
 
   return (
     <div class={`container-fluid p-0 ${styles.nav_bar}`}>
       <nav
         class={`${styles.navbar} ${styles.navbar_expand_lg} bg-none navbar-dark py-3`}
       >
-        <a href="index.html" class={`${styles.navbar_brand} px-lg-4 m-0`}>
-          <h1 class="m-0 display-4 text-uppercase text-white">Next-Coffee</h1>
-        </a>
+        <div className="d-flex align-items-center position-relative">
+          <Link href="/" className={`${styles.navbar_brand} px-lg-4 m-0`}>
+            <h1 className="m-0 display-4 text-uppercase text-white">
+              Next-Coffee
+            </h1>
+          </Link>
+
+          <input
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+            type="text"
+            onKeyDown={searchHandlerWithEnter}
+            className={styles.search_input}
+            placeholder="Search..."
+          />
+          <FontAwesomeIcon
+            onClick={searchHandlerWithIcon}
+            className={styles.search_icon}
+            icon={faSearch}
+          />
+        </div>
+
         <button
           type="button"
           class={`${styles.navbar_toggler}`}
@@ -52,10 +100,10 @@ function Navbar() {
               </a>
               <div class={`${styles.dropdown_menu} ${styles.text_capitalize}`}>
                 <Link href='/reservation' class={`${styles.dropdown_item}`}>
-                Reservation
+                  Reservation
                 </Link>
                 <Link href='/testimonial' class={`${styles.dropdown_item}`}>
-                Testimonial
+                  Testimonial
                 </Link>
               </div>
             </div>
--- pages/search/index.js
+++ pages/search/index.js
@@ -1,9 +1,32 @@
+import PageHeader from '@/components/modules/PageHeader/PageHeader'
+import Routes from '@/components/templates/Search/Routes'
 import React from 'react'
 
-function Searech() {
+function Search({data}) {
+
+  
   return (
-    <h1>Searech Page</h1>
+    <>
+      <PageHeader />
+      <Routes data={data} />
+    </>
   )
 }
 
-export default Searech
+export async function getServerSideProps(context) {
+
+  const { query } = context
+
+  const res = await fetch('http://localhost:4000/menu')
+  const data = await res.json()
+
+  const searchResult = data.filter(item => item.type.toLowerCase().includes(query.q.toLowerCase()) || item.title.toLowerCase().includes(query.q.toLowerCase()))
+  console.log(searchResult);
+  return {
+    props: {
+      data : searchResult,
+    },
+  }
+}
+
+export default Search
